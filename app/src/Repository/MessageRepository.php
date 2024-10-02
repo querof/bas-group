d43b10b885e4f97764ed6229e8ad8404cfd21a16
Frank Quero <querof@gmail.com>
2024-10-02 16:18:22 +0200

    BAS-007 - Add DeleteExpiredMessagesCommand

--- app/src/Repository/MessageRepository.php
+++ app/src/Repository/MessageRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Message;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -21,28 +22,13 @@ class MessageRepository extends ServiceEntityRepository
         parent::__construct($registry, Message::class);
     }
 
-//    /**
-//     * @return Message[] Returns an array of Message objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Message
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+     public function findExpiredMessages(): array
+    {
+        $now = new DateTimeImmutable();
+        return $this->createQueryBuilder('m')
+            ->where('m.expirationDate <= :now')
+            ->setParameter('now', $now)
+            ->getQuery()
+            ->getResult();
+    }
 }
